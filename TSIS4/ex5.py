f451170e1d0ba48289724a104ea89072ccfb319c
Beknur <b_zhorabel@kbtu.kz>
2024-03-05 16:25:01 +0500

    TSIS4

---
+++ TSIS4/ex5.py
@@ -0,0 +1,21 @@
+import json
+
+with open("sample_data.json", "r") as f:
+    data = json.load(f)
+
+    print("Interface Status")
+    print("================================================================================")
+    print("DN                                                 Description           Speed    MTU")  
+    print("-------------------------------------------------- --------------------  ------  ------")
+
+
+    for interface in data.get('imdata', []):
+        l1phy = interface.get('l1PhysIf', {})
+        att = l1phy.get('attributes', {})
+    
+        name = att.get('name', '-')
+        description = att.get('description', '-')
+        speed = att.get('speed', '-')
+        mtu = att.get('mtu', '-')
+    
+        print(f'{name:<50} {description:<20} {speed:<8} {mtu:<6}')
